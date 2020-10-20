2b83165efdfa812fba6ef632d7b92efec3af7ed4
java-861 <72907619+java-861@users.noreply.github.com>
2020-10-20 11:18:23 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -2,7 +2,11 @@ class Calc{
    public static int add(int a,int b){
     return (a+b);
     }
+   public static int subtract(int a,int b){
+    return (a-b);
+    }
     public static void main(String args[]){
         System.out.println(add(10,20));
+        System.out.println(subtract(20,10));
      }
 }
